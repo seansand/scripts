73d4ebc843473edbfb9eac9e16c56c1508eb7dcf
Sean Sandquist <sean.sandquist@tr.com>
2018-08-21 17:59:43 -0500

    Improved ExcelToHtmlCaller.

--- ExcelToHtmlCaller.groovy
+++ ExcelToHtmlCaller.groovy
@@ -3,54 +3,67 @@
 
 import java.util.regex.*;
 
-String excelFileIn = "x:\\Mapped\\WFFL\\2017.xlsx";
-String htmlFile1 = "x:\\Mapped\\WFFL\\2017-orig.html";
-String htmlFile2 = "x:\\Mapped\\WFFL\\2017.html";
-String changesFileName = "x:\\Mapped\\WFFL\\2017-changes-MILLIS.txt";
+YEAR = "2018"
+
+String excelFileIn = "x:\\Mapped\\WFFL\\${YEAR}.xlsx";
+String htmlFile1 = "x:\\Mapped\\WFFL\\${YEAR}-orig.html";
+String htmlFile2 = "x:\\Mapped\\WFFL\\${YEAR}.html";
+String changesFileName = "x:\\Mapped\\WFFL\\${YEAR}-changes-MILLIS.txt";
 changesFileName = changesFileName.replaceAll("MILLIS", "" + System.currentTimeMillis());
 File changesFile = null;
 Set<String> changesSet = new LinkedHashSet<String>();
 
 
-Integer DELAY_SECONDS = 20
+Integer DELAY_SECONDS = 18
 
-Boolean RECORD_CHANGES = true
+RECORD_CHANGES = true
 
 while(true) 
 {
-    println("converting $excelFileIn to $htmlFile1");
-    org.apache.poi.ss.examples.html.ExcelToHtml.main(excelFileIn, htmlFile1)
-    modifyHtml(htmlFile1, htmlFile2, RECORD_CHANGES, changesSet)
-
-    if (RECORD_CHANGES)
+    try 
     {
-       println("Changes file = $changesFileName");
-       changesFile = new File(changesFileName);
-       changesFile.write("");
-       changesFile.append("CHRON LIST\n");
-       changesSet.each{print("."); changesFile.append(it + "\n")}
-       
-       List<String> alphaList = new ArrayList<String>(changesSet);
-       alphaList = alphaList.sort();
-       changesFile.append("\nALPHA LIST\n");
-       
-       alphaList.each{changesFile.append(it + "\n")}
+
+        println("converting $excelFileIn to $htmlFile1");
+        org.apache.poi.ss.examples.html.ExcelToHtml.main(excelFileIn, htmlFile1)
+        modifyHtml(htmlFile1, htmlFile2, changesSet)
+
+        if (RECORD_CHANGES)
+        {
+           println("Changes file = $changesFileName");
+           changesFile = new File(changesFileName);
+           changesFile.write("");
+           changesFile.append("CHRON LIST\n");
+           changesSet.each{print("."); changesFile.append(it + "\n")}
+           
+           List<String> alphaList = new ArrayList<String>(changesSet);
+           alphaList = alphaList.sort();
+           changesFile.append("\nALPHA LIST\n");
+           
+           alphaList.each{changesFile.append(it + "\n")}
+        }
+    }
+    catch (Exception e) 
+    {
+        e.printStackTrace()
+        println("Continuing to loop...")
     }
     
-    println("OK to Ctrl-C to quit");
+    print((new Date()).toString())
+    println(" . . . OK to Ctrl-C to quit");
     
     Thread.sleep(DELAY_SECONDS * 1000)
 }
 
 
-void modifyHtml(String inFileName, String outFileName, def RECORD_CHANGES, def changesSet)
+void modifyHtml(String inFileName, String outFileName, def changesSet)
 {
    String replaceThis = "<head>";
-   String replaceWith = '<head><meta http-equiv="refresh" content="25"/><TITLE>2017 WFFL Auction</TITLE>'
+   String replaceWith = '<head><meta http-equiv="refresh" content="20"/><TITLE>' + YEAR + ' WFFL Auction</TITLE>'
 
    File inFile = new File(inFileName)
    File outFile = new File(outFileName)
-   outFile.write("")
+   
+   String buffer = ""
    
    inFile.eachLine()
    {
@@ -59,12 +72,13 @@ void modifyHtml(String inFileName, String outFileName, def RECORD_CHANGES, def c
       {
          line = replaceWith
       }
-      recordChanges(line, RECORD_CHANGES, changesSet)      
-      outFile.append(line + "\n");
+      recordChanges(line, changesSet)      
+      buffer += line + "\n"
    }
+   outFile.write(buffer)
 }
 
-void recordChanges(String line, def RECORD_CHANGES, def changesSet)
+void recordChanges(String line, def changesSet)
 {
    Pattern p = Pattern.compile('<td class=style_.*text-align: left;">(.*)</td>');
 
