333a35fa33cc06363f01f36bc998d5170ee1fb66
Sean Sandquist <seansand@uwalumni.com>
2021-03-29 22:26:24 -0500

    Latest

--- TimesFive.groovy
+++ TimesFive.groovy
@@ -2,291 +2,50 @@
 
 String str = '''
 
-The Horror from the South Pacific, age 33, circus geek (0/1 SAN to see)
-STR 12 CON 9 SIZ 13 INT 9 POW 7
-DEX 12 APP 2 EDU 3 SAN 0 HP 11
+NIGHT WATCHMAN, age 32, Inattentive Guard 
+STR 13 CON 11 DEX 10 APP 8 SIZ 12 
+INT 8 POW 10 EDU 10 SAN 50 HP 12
+Weapon: Night-stick 52%, damage 1D6+1D4 
+Skills: Sleep on Duty 50%, Brawl 48%, Listen 40%, Spot Hidden 30%
+
+CLARENCE RODGERS, age 27, Master of disguise
+STR 13 CON 13 SIZ 14 DEX 14 APP 13 
+INT 15 POW 14 EDU 16 SAN 0 HP 14
+Weapon: .32 Revolver 65%, damage 1D8 
+Skills: Anthropology 50%, Archaeology 55%, Astronomy 10%, Cthulhu Mythos 25%, Disguise 90%, Fast Talk 70%, Hide 65%, History 70%, Library Use 60%, Listen 50%, Occult 50%, Psychology 48%, Quick Change 75%, Sneak 60%, Spot Hidden 60%
+
+RUKUKIK (EDITH BRYANT), age of body 34, 
+STR 10 CON 14 SIZ 12 DEX 16 APP 11 
+INT 20 POW 14 SAN 0 HP 13
+Weapon: .32 Revolver 45%, damage 1D8
+Skills: History 99%, Knowledge of Paul LeMond's Mind 40%, Disguise 50%, Listen 60%, Hide 60%, Use Tabula Rasa Device 80%. Languages: English 50%, Pnakotic 90%.
+
+BUGSY WEXLER, age 32, Da Boss 
+STR 15 CON 15 DEX 13 APP 8 SIZ 15
+INT 13 POW 10 EDU 6 SAN 40 HP 15
+Damage Bonus: +1D4. 
+Weapons: .45 Automatic 85%, damage 1D10+2 
+Fist/Brawl 85%, damage 1D3+1D4 Head Butt 55%, damage 1D4+1D4 Skills: Bargain 50%, Drive Automobile 50%, Hide 75%, Persuade 50%, Pick Pocket 50%, Sneak 75%.
+
+BUGSY'S TYPICAL THUGS 
+STR 15 CON 13 SIZ 16 INT 10 POW 8
+DEX 13 APP 6 EDU 5 SAN 40 HP 15
 Damage Bonus: +1D4.
-Weapons: 
-Claws (x2) 40%, 1D4 each; 
-Bite 20%, 1D4.
-Skills: Dodge 35%, Hide 55%, Listen 60%,
-Rant and Rave 90%, Sneak 40%, Spot Hidden 40%.
+Weapons: .38 Automatic 80%, damage 1D10 
+Fist/Brawl 85%, damage 1D3+1D4
+Skills: Drive Automobile 40%, Hide 35%, Move Quietly 30%, Pick Pocket 25%.
 
------
+TYPICAL SANITARIUM GUARD 
+STR 15 CON 14 SIZ 13 INT 9 POW 11
+DEX 13 APP 9 EDU 10 SAN 55 HP 14
+Damage Bonus: +1D4. Weapons: Night-stick 50%, damage 1D6+1D4 Brawl 65%, 
+Skills: Listen 45%, Spot Hidden 55%. 
 
-Jeb Prescott, age 22, Jealous sweetheart
-STR 15 CON 14 SIZ 15 INT 10 POW 10
-DEX 11 APP 8  EDU 5  SAN 44 HP 15
-Damage Bonus +1D4.
-Weapons: 
-Brawl 85% 1D3+1D4
-12-gauge Double-Barrelled Shotgun 80%, 4D6/2D6/1D6.
-Skills: Dodge 75%.
+GUARD DOG 
+STR 10 CON 12 SIZ 7 POW 7 DEX 13 HP 10 
+Weapons: Bite 40%, damage 1D6 
+Skills: Listen 75%, Scent 90%.
 
------
-
-Cousins and friends, ages 15-31
-       STR CON SIZ POW DEX  HP
-ZEKE    14  15  11  12  17  13
-JOSH    16  14   9  12   9  12
-CALVIN  13  11  12  10  10  12
-DOUGLAS 15  13  10  14  14  12
-AARON   16  11   9  13  13  10
-JODY    15  15  10   9  15  13
-Damage Bonus: +1D4
-Weapons: 
-Brawl 65%, 1D3+1D4 
-Knife 55%, 1D3; 
-Club 60%, 1D6;
-Shotgun 65%, 4D6/2D6/1D6.
-Skills: Dodge 55%.
-
------
-
-Gorgo the Ape, chimpanzee
-STR 20 CON 13 SIZ 11 INT 7 POW 8
-DEX 16 HP 12  Move 8
-Weapon.: Bite 30%, 1D4+1D4; 
-Paws (x2) 40%, 1D4+1D4 each.
-Skills: Climb 85%, Spot Hidden 30%.
-
------
-
-Scotty Andrewskl, age 27, maniac clown (0/1 SAN to see attack)
-STR 13 CON 10 SIZ 15 INT 13 POW 12
-DEX 10 APP 12 EDU 12 SAN 55 HP 13
-Damage Bonus: +1D4.
-Weapons: 
-Brawl 40%, 1D3+1D4
-Meat cleaver 40%, 1D6+1
-Skills: Dodge 35%. Listen 30%, Sneak 20%, Spot Hidden 35%
-
------
-
-Mike and Rex, The Fabulous Farley Steeds
-     STR CON SIZ POW DEX HP
-MIKE  28  13  27   9  13 20
-REX   28  12  26  10  12 19
-Weapons: Bite 10% 1D10; 
-Kick 15%,1D8+2D6; 
-Rear and Plunge 25%,2D8+2D6; 
-Trample 25%, 4D6 (downed foes only)
-Armor: 1 point of hide
-Skills: Jump 55%
-
------
-
-Nambuto "The Watusl Giant" age 28, aka Big John Newland
-STR 16 DEX 14 CON 16 APP 13 POW 13
-SIZ 19 EDU  6 INT 12 SAN 65 HP 18
-Damage Bonus: +1D6,
-Weapons: 
-Two-Handed Spear 45%, 1D8+1: 
-Thrown Spear 85%, 1D8+1; 
-Brawl 75%, 1D3+1D6
-Skills: Climb 65%, Dodge 70%, Jump 85%, Listen 55%, Occult
-15%, Poker 70%, Sneak 65%, Spot Hidden 50%, Throw 85%.
-
------
-
-Marcus the Lion
-STR 19 CON 10 SIZ 18 INT 5 POW 11
-DEX 17 Move 10 HP 14
-Weapons: 
-Bite 35%, 1D10+1D6; 
-Claws (x2) 55%, 1D6+1D6 each; 
-Rake 80%, 4D6.
-Armor: 2 point skin.
-Skills: Hide 50%, Listen 45%, Scent Prey 50%, Sneak 70%.
-The lion attacks with two claws and one bite per round against
-a single opponent. If both claws hit, it then rakes with its hind
-legs while biting at the same time.
-
------
-
-Clyde Court, age 35, lion tamer
-STR 10 CON 14 SIZ 12 INT 13 POW 15
-DEX 17 APP 12 EDU 13 SAN 73 HP 13
-
-Weapons: 
-Whip 85%, 1D3 (plus entanglement or immobilization of a limb on an extreme roll: STR VS. STR needed to break free); 
-.45 Revolver 40%, 1D10+2; 
-Brawl 55%, 1D3
-Skills: Climb 60%, Credit Rating 40%, Dodge 70%, First Aid
-50%, Jump 55%, Lion-Taming 85%, Listen 50%, Spot Hidden
-40%, Throw 40%, Track 55%, Zoology 55%.
-
------
-
-Ted Nichols, age 47, carnival owner and ringmaster
-STR 12 CON 12 SIZ 14 INT 14 POW 15
-DEX 11 APP 13 EDU 14 SAN 75 HP 13
-Damage Bonus: +1D4,
-Weapons: 
-Brawl 65%, 1D3+1D4  
-Club 40%, 1D6+1D4
-Whip 40%, 1D3;
-.38 Revolver 30%, 1D10,
-Skills: Accounting 40%, Bargain 55%. Climb 50%, Credit Rating
-45%, Debate 35%, Dodge 40%, Drive Automobile 30%,
-Talk 70%, Jump 35%, Law 35%. Listen 55%, Make Maps 25%, 
-Mech. Repair 35%, Oratory 90%, Psychology 25%,
-Ride 15%, Spot Hidden 40%, Zoology 10%,
-
------
-
-Lucius "Dots" O'Dowd, age 39, freak show owner
-STR 14 CON 12 SIZ 15 INT 13 POW 12
-DEX 11 APP 10 EDU 10 SAN 36 HP 14
-Bonus: +1D4.
-Weapons: 
-Brawl 70% 1D3 (+2 with brass knuckles)
-Large Switchblade 45%, 1D4
-Club 50%,
-.38 Revolver 40%, 1D10
-Skills: Bargain 65%, Climb 50%, Dodge 40%, Drive Automobile
-35%, Electrical Repair 35%, Fast Talk 75%, Hide 45%, Law
-Listen 30%. Mechanical Repair 45%, Oratory 60%, Pick 30%, 
-Psychology 25%, Sneak 25%, Spot Hidden 50%
-
------
-
-The Fish Boy, age 9 months (Lose 0/1D3 SAN to see)
-STR 3 CON 9 SIZ 2 INT 4 POW 6
-DEX 7 APP 1  Move 2/6 Swimming HP 6
-Weapons: 
-Tail-lash 30%, 1D3-1
-Bite 5%, 1D3-1
-Claws 10%, 1D2-1
-Armor: 1 point of soft scales
-Skills: Swim 100%
-
------
-
-Brad Rayburn, age 31, head roustabout
-STR 14 CON 10 SIZ 15 INT 13 POW 12
-DEX 12 APP 13 EDU 11 SAN 51 HP 13
-Damage Bonus: +1D4.
-Weapons: Brawl 85%, 1D3+1D4
-Tire Iron 60%, 1D8.
-Skills: 45%, Electrical Repair 55%. Law 20%, Listen
-60%, Repair 75%, Spot Hidden 50%, Throw 60%,
-
------
-
-Roustabouts, ages 15-55
-       STR CON SIZ POW DEX HP
-HARLAN  13  14   4  11 17  14
-PAUL    15  14  12  11  9  13
-EDDIE   13  12  16  10 10  14
-MASON   12  12  13   9 14  13
-JOE     11  10  15  12 13  13
-NODDY   14  15  15   9 15  15
-Damage Bonus: +1D4.
-Weapons: 
-Brawl 65%, 1D3+1D4
-Knife 45%, 1D3+1D4 
-Club 40%, 1D6+1D4
-Skills: 35%, Electrical Repair 30%, Listen 30%, Mech.
-Repair 55%, Spot Hidden 30%, Throw 35%
-
------
-
-Jacob Marsh, age 29, deep one hybrid & chief negotiator
-STR 10 CON 13 SIl11 INT 13 POW 13
-DEX 13 APP 9 EDU 12 SAN 0  HP 12
-Weapons: 
-None carried. All at base percentages.
-Skills: Aooounting 65%, Bargain 65%, Chemistry 20%, Credit
-Rating 55%, Cthulhu Mythos 30%, Factory Management 55%,
-Geology 45%, Mechanical Repair 30%, Oratory 75%, Psychology 55%, 
-Spot Hidden 45%.
-
------
-
-Wilbur Babson, age 43, deep one hybrid, father of the Fish Boy
-STR 15 CON 13 SIZ 14 INT 12 POW 12
-DEX 11 APP 8 EDU 8 SAN 0 HP 14
-Weapons: 
-Brawl 65%, 1D3
-.45 Revolver 30%, 1D10+2
-Skills: Bargain 35%, Dodge 25%, Drive Automobile 30%, Fast
-Talk 30%, Hide 20%, Listen 30%, Sneak 20%, Spot Hidden
-50%, Swim 65%.
-
------
-
-Reggie Bablon, age 31, deep one hybrid
-and cousin of the Fish Boy
-STR 11 CON 12 SIZ 14 INT 12 POW 10
-DEX 13 APP 9 EDU 8 SAN 0 HP 13
-Damage Bonus: +1D4.
-Weapons: 
-Brawl 55%, 1D3+1D4 
-Fighting Knife 30%, 1D4+2+1D4
-Baseball Bat 40%, 1D8+1D4
-Skills: Bargain 20%, Dodge 30%, Drive Automobile 45%, Fast
-Talk 45%, Hide 30%, Listen 30%, Sneak 45%, Spot Hidden
-40%, Swim 65%
-
------
-
-Miscellaneous deep one hybrids, ages 20-45
-           STR CON SIZ POW DEX  HP
-F. PIERCE   12  11  13  12  16  12
-F. GILMAN   15  14  16   9  13  15
-I. BABSON   11  13  14  14  10  13
-S. SARGENT  13   9  13  11  13  11
-T. GILMAN   13  12  12  13   6  12
-N. PIERCE   13  15  15  11  14  15
-Damage Bonus: +1D4
-Weapons: 
-Brawl 55%, 1D3+1D4
-Knife 35%, 1D4+1D4
-Club 45%, 1D6+1D4
-Skills: Dodge 30%, Hide 25%, Sneak 20%, Spot Hidden 30%, Swim 60%.
-
------
-
-Professor Jacob Handy, age 27, experimental biologist
-STR 9 CON 9 SIZ 12 INT 16 POW 12
-DEX 9 APP 12 EDU 19 SAN 52 HP 11
-Weapons: 
-Scalpel 75%, 1D3 (has twice normal chance to Impale)
-Skills: Accounting 15%, Anthropology 25%, Biology 70%, Botany
-20%, Chemistry 15%, Credit Rating 45%, Debate 45%,
-English 95%, First Aid 50%, History 45%, Latin 50%, Library
-Use 55%, Psychology 30%, Spot Hidden 40%, Zoology 70%
-
------
-
-Detective Douglas Mooney, age 36, Aylesbury P.D.
-STR 9 CON 10 SIZ 14 INT 15 POW 12
-DEX 8 APP 9  EDU 15 SAN 58 HP 12
-
-Weapons: Brawl 55%, 1D3 
-Nightstick 35%, 1D6
-.38 Revolver 60%, 1D10
-Skills: Debate 40%, Dodge 35%, Drive Auto 40%, 
-First Aid 40%, Law 55%, Listen 60%, Oratory 55%, 
-Psychology 50%, Spot Hidden 55%.
-
------
-
-Police Officers, ages 23-52
-       STR CON SIZ POW DEX HP
-JONES   13  11  12  12 12  12
-SMITH   14  14  13  12 11  14
-ADAMS   11  12  16  10 14  14
-WRIGHT  12  12  13  11 10  13
-JOHNSON 13  10  12  10  9  11
-MILLER  14  15  11   9 15  13
-Damage Bonus: +1D4.
-Weapons: 
-Brawl 55%, 1D3; 
-.38 Revolver 55%, 1D10.
-Skills: Dodge 45%, Law 40%, Listen 30%, Psychology 35%,
-Spot Hidden 35%, Track 35%.
 '''
 
 str.eachLine() {
